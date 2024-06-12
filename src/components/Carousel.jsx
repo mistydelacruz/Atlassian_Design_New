081f5f56909a5fedc0cdeabcf94b509e0d9cb8cc
mistydelacruz <misty.dela.cruz1@gmail.com>
2024-06-12 13:39:01 -1000

    Added the image carousel code.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,50 +3,69 @@
  * Created on: 4/15/2024
  * Last Modified: 5/20/2024
  * Updates:
- * Carousel is working.
- * Next: make it looping or endless.
+ * Carousel is working
+ * Next: make it looping or endless
  ***************************************/
 
 import React, { useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/carouselData";
+import ArrowLeftIcon from "@atlaskit/icon/glyph/arrow-left";
+import ArrowRightIcon from "@atlaskit/icon/glyph/arrow-right";
 
 function Carousel() {
   const [currImg, setCurrImg] = useState(0);
-  {
-    /*useState to change between images*/
-  }
+
+  /***************************************
+   *     CAROUSEL LEFT CLICK HANDLE      *
+   * Uses ternary logic
+   * Checks if currImg is EQUAL to 0
+   * If so, set currImg to images.length (3) - 1 (last img in the array (2))
+   * Else, keep clicking left
+   ***************************************/
+  const handleLeftClick = () => {
+    setCurrImg((currImg) => (currImg === 0 ? images.length - 1 : currImg - 1));
+  };
+
+  /***************************************
+   *     CAROUSEL RIGHT CLICK HANDLE     *
+   * Uses ternary logic
+   * Checks if currImg is EQUAL to images.length (3) - 1 (last img in the array (2))
+   * If so, set currImg to 0
+   * Else, keep clicking right
+   ***************************************/
+  const handleRightClick = () => {
+    setCurrImg((currImg) => (currImg === images.length - 1 ? 0 : currImg + 1));
+  };
+
+  /***************************************
+   *       IMAGE CAROUSEL COMPONENT      *
+   * Carousel is divided into 3 parts
+    - Left side
+    - Inner (center)
+    - Right side
+   * Left side controls the left arrow for navigating (same for right side)
+   * The inner portion displays the images as background images
+  ****************************************/
   return (
     <>
       <div className="carousel">
-        {" "}
-        {/*Divider for the logo*/}
+        {/***** INNER CAROUSEL *****/}
         <div
           className="carousel-inner"
           style={{ backgroundImage: `url(${images[currImg].img})` }}
         >
-          {" "}
-          {/*Divider for the inner carousel*/}
-          <div
-            className="carousel-left"
-            onClick={() => {
-              currImg > 0 && setCurrImg(currImg - 1);
-            }}
-          >
-            {" "}
-            {/*Divider for the left arrow icon, checks if currImg > 0 AND currImg decreases by 1*/}
-            {/* <KeyboardArrowLeftIcon style={{ fontSize: 30 }} /> */}
+          {/***** LEFT ARROW *****/}
+          <div className="carousel-left" onClick={handleLeftClick}>
+            <ArrowLeftIcon></ArrowLeftIcon>
           </div>
+
+          {/***** CENTER *****/}
           <div className="carousel-center"></div>
-          <div
-            className="carousel-right"
-            onClick={() => {
-              currImg < images.length - 1 && setCurrImg(currImg + 1);
-            }}
-          >
-            {" "}
-            {/*Divider for the right arrow icon, checks if currImg < images.length - 1 AND currImg increases by 1*/}
-            {/* <KeyboardArrowRightIcon style={{ fontSize: 30 }} /> */}
+
+          {/***** RIGHT ARROW *****/}
+          <div className="carousel-right" onClick={handleRightClick}>
+            <ArrowRightIcon></ArrowRightIcon>
           </div>
         </div>
       </div>
